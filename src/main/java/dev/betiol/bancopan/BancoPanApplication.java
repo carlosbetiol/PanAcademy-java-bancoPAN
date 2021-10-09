56e77d9199c2de64d7d69e11a678761a9b454ce9
Carlos Betiol <carlos@sistelpa.com>
2021-10-09 14:07:19 -0300

    start banco PAN example application to PAN Academy

---
+++ src/main/java/dev/betiol/bancopan/BancoPanApplication.java
@@ -0,0 +1,13 @@
+package dev.betiol.bancopan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoPanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BancoPanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/betiol/bancopan/BancoPanApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.betiol.bancopan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoPanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
